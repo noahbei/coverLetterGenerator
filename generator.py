2cadbd1c84a952f1ef8539605e17657b937cced8
Noah Beidelman <nbeidelman9@gmail.com>
2024-04-01 20:34:52 -0700

    add input for school  name, github, and skills

--- generator.py
+++ generator.py
@@ -11,14 +11,18 @@ styleN = ParagraphStyle(
     fontName='Helvetica'
 )
 
-def createLetter(applicantName, companyName):
+def createLetter(applicantName, companyName, schoolName, github, skills):
     story = []
+    skills_str = ", ".join(skills[:-1]) + ", and " + skills[-1]
 
     #add some flowables
     story.append(Paragraph(f'Dear {companyName},', styleN))
     story.append(Spacer(1, 0.2 * inch))
-    story.append(Paragraph(f'I am writing to express my interest in the software engineering role at {companyName}. As a dedicated and motivated computer science student with a passion for software engineering, I am excited about the opportunity to contribute my skills and knowledge to your dynamic team. Throughout my academic journey, I have gained a solid foundation in key programming languages such as C/C++, Python, HTML, CSS, and JavaScript, and I have successfully applied these skills in various hands-on projects, big and small. My strong problem-solving abilities and commitment to continuous learning make me confident in my ability to thrive in a challenging software engineering environment. I am eager to contribute my technical expertise to help drive your mission. Thank you for considering my application. I look forward to the possibility of discussing how my skills align with your needs.', styleN))
+    story.append(Paragraph(f'I am writing to express my interest in the software engineering role at {companyName}. As a dedicated and motivated computer science student at {schoolName} with a passion for software engineering, I am excited about the opportunity to contribute my skills and knowledge to your dynamic team. Throughout my academic journey, I have gained a solid foundation in key programming languages and technologies, including {skills_str}.', styleN))
     story.append(Spacer(1, 0.2 * inch))
+    story.append(Paragraph(f'I have successfully applied these skills in various hands-on projects, big and small. My projects, along with their source code, are available on my GitHub account at {github}, showcasing my dedication and proficiency in software engineering. My strong problem-solving abilities and commitment to continuous learning make me confident in my ability to thrive in a challenging software engineering environment. I am eager to contribute my technical expertise to help drive your mission. Thank you for considering my application. I look forward to the possibility of discussing how my skills align with your needs.', styleN))
+    story.append(Spacer(1, 0.2 * inch))
+
     story.append(Paragraph("Sincerely,", styleN))
     story.append(Paragraph(applicantName, styleN))
 
--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from generator import createLetter
 if __name__ == "__main__":
     company_name = input("Input Company Name: ")
     applicant_name = "Noah Beidelman"
-    createLetter(applicant_name, company_name)
+    school_name = "Cal State University Fullerton"
+    github = "github.com/noahbei"
+    skills = [
+        "HTML", "CSS", "JavaScript", "React", "React Native", "C/C++", "Python",
+        "SQL", "MySQL", "Firebase", "Git", "GitHub", "Bootstrap", "EJS", "Figma",
+        "Linux", "Docker", "Selenium", "Agile methodologies"
+    ]
+    createLetter(applicant_name, company_name, school_name, github, skills)
