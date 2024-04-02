2dd7c8dc8ab06ba1ef5e057794d9689afd59f634
noahbei <nbeidelman9@gmail.com>
2024-04-01 20:03:57 -0700

    add company name promt to input

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from generator import createLetter
 
 if __name__ == "__main__":
-    company_name = input()
+    company_name = input("Input Company Name: ")
     applicant_name = "Noah Beidelman"
     createLetter(applicant_name, company_name)
